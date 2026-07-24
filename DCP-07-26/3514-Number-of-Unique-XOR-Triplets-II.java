6602f1d04ca557c953e21300df98d4f452cafe90
Yashbardwaj10 <145279002+Yashbardwaj10@users.noreply.github.com>
2026-07-24 23:00:33 +0530

    [3514] [Time Beats: 18.18%] [Memory Beats: 30.91%] - LeetPush

--- DCP-07-26/3514-Number-of-Unique-XOR-Triplets-II.java
+++ DCP-07-26/3514-Number-of-Unique-XOR-Triplets-II.java
@@ -1,21 +1,43 @@
-import java.util.*;
-
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        HashSet<Integer> values = new HashSet<>();
-        for (int x : nums) values.add(x);
-        int[] a = new int[values.size()];
-        int idx = 0;
-        for (int x : values) a[idx++] = x;
-        HashSet<Integer> ans = new HashSet<>();
-        int m = a.length;
-        for (int i = 0; i < m; i++) {
-            for (int j = i; j < m; j++) {
-                for (int k = j; k < m; k++) {
-                    ans.add(a[i] ^ a[j] ^ a[k]);
-                }
-            }
+
+        // Remove duplicates since only unique values affect the XOR results.
+        Set<Integer> uniqueValues = new HashSet<>();
+
+        for(int num:nums)
+        {
+            uniqueValues.add(num);
         }
-        return ans.size();
+        
+        ///nums[i] <= 1500, so every XOR value lies in the range [0, 2047].
+        final int MAX_XOR = 2048;
+
+        boolean[] reachablePairXor = new boolean[MAX_XOR];
+        boolean[] reachableTripletXor = new boolean[MAX_XOR];
+
+        // Compute all XOR values obtainable using any two numbers.
+        for(int first:uniqueValues)
+        {
+            for(int second:uniqueValues)
+                reachablePairXor[first^second] = true;
+        }
+
+        //Extend every reachable pair XOR with a third number
+        for(int xorValue = 0; xorValue < MAX_XOR; xorValue++)
+        {
+            if(!reachablePairXor[xorValue])
+                continue;
+            for(int third: uniqueValues)
+                reachableTripletXor[xorValue^third] = true;
+        }
+
+        //Count all reachable triplet XOR values.
+        int uniqueTriplets = 0;
+        for(boolean reachable : reachableTripletXor)
+        {
+            if(reachable)
+                uniqueTriplets++;
+        }
+        return uniqueTriplets;
     }
 }
